5755af2c54d254a51b0ba5f7617360b42927c418
Damilare Jolayemi <djolayemi@gmail.com>
2021-03-29 17:18:11 +0100

    Update

---
+++ src/Admin.java
@@ -0,0 +1,22 @@
+public class Admin {
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+    private String staffID;
+
+    public Admin(String name, String phoneNumber, String emailAddress, String staffID) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.staffID = staffID;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "name='" + name + '\n' +
+                "phoneNumber='" + phoneNumber + '\n' +
+                ",emailAddress='" + emailAddress + '\n' +
+                '}';
+    }
+}
