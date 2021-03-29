59dd0304419aad999b0615b6291ca2c97b5333f3
Damilare Jolayemi <djolayemi@gmail.com>
2021-03-29 17:19:26 +0100

    Update

---
+++ src/Seat.java
@@ -0,0 +1,20 @@
+public class Seat {
+    private int seatNumber;
+    private SeatClass seatType;
+
+    public void assignSeatNumber(int seatNumber){
+        this.seatNumber = seatNumber;
+    }
+
+    public void assignSeatType(SeatClass seatType){
+       this.seatType = seatType;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public SeatClass getSeatType() {
+        return seatType;
+    }
+}
